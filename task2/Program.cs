e138dc97fa10298a6561176acee33e79f05f167f
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-23 17:41:30 +0300

    квадрат числа

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,14 +1,14 @@
-﻿Console.WriteLine("Введите число,обзначающее номер дня недели");
-int n = int.Parse(Console.ReadLine());
- 
-int a = 6;
-if(n < a)
+﻿Console.WriteLine("Введите первое число");
+int a = int.Parse(Console.ReadLine());
+ Console.WriteLine("Введите второе число");
+int b = int.Parse(Console.ReadLine());
+if(b*b == a)
 
 {
-    Console.WriteLine("Будничный день");  
+    Console.WriteLine(a + " " + "квадрат второго числа");  
 }
 else {
-    Console.WriteLine("Выходной");
+    Console.WriteLine(a +" "+ "не является квадратом числа" + " " + b);
 }
      
  
