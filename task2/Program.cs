7ccb4314daca2553611ca9202acf8b1dd35702ba
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-23 17:25:15 +0300

    Daysoff

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число,обзначающее номер дня недели");
+int n = int.Parse(Console.ReadLine());
+ 
+int a = 6;
+if(n < a)
+
+{
+    Console.WriteLine("Будничный день");  
+}
+else {
+    Console.WriteLine("Выходной");
+}
+     
+ 
