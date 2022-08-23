f01d96003300d56ad520b0258d480d7e829d9ca6
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-23 20:01:33 +0300

    Possible coordinates of the quarter

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите номер четверти");
+int n = int.Parse(Console.ReadLine());
+if (n == 1)
+{
+    Console.WriteLine("Возможные координаты: x > 0, y > 0");
+}
+if (n == 2)
+{
+  Console.WriteLine("Возможные координаты: x < 0, y > 0"); 
+}
+if (n == 3)
+{
+    Console.WriteLine("Возможные координаты: x < 0, y < 0");
+}
+if (n == 4)
+{
+  Console.WriteLine("Возможные координаты: x > 0, y < 0"); 
+}
