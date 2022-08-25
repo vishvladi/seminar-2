67cfd2bfef70c81be24acbc8a985da7e11ae162e
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-25 10:39:21 +0300

    Multiplying of numbers

---
+++ task9/Program.cs
@@ -0,0 +1,9 @@
+﻿//Multiplying numbers from 1 to N
+int n = new Random().Next(1, 10);
+Console.WriteLine(n);
+int count = 1;
+int mult = 1;
+while (count <= n){
+mult = mult * count;
+count++;}
+Console.WriteLine(mult);
