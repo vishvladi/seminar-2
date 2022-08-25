5b5b3237ce7b7069ad96543004dc72b010d77db4
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-25 09:15:05 +0300

    cubes of numbers

---
+++ task5/Program.cs
@@ -0,0 +1,9 @@
+﻿//Найти кубы чисед от 1 до N
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int count = 1;
+while (count <= n)
+{
+ Console.WriteLine(count * count * count); 
+ count++;  
+}
