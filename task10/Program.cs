a951f03704af857a8f1c8fdaecfc1ed64ba1f25e
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-25 10:51:31 +0300

    Cubes

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Показать кубы чисел заканчивающихся на четную цифру
+int n = 100;
+int count = 1;
+int cube = 1;
+while (count <= n)
+{ cube = count * count * count;
+  if(cube%2 == 0){
+    Console.WriteLine(cube);
+  }
+  count++;  
+}
