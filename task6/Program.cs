b070b332892ba8d60e122896bf051c492378e960
Macbook Pro 15 <macbookpro@MacBook-Pro-Macbook.local>
2022-08-25 09:26:43 +0300

    sum of numbers

---
+++ task6/Program.cs
@@ -0,0 +1,11 @@
+﻿//Сумма чисел от 1 до А
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int count = 1;
+int sum = 0;
+while (count <= a)
+{
+    sum = sum + count;
+    count++;
+}
+Console.WriteLine(sum);
